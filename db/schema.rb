b5fc27bbe846bc242992e1e7aacc32f693957221
orolyana <ibrahimabas97@gmail.com>
2020-01-19 15:01:52 +0300

    db

--- db/schema.rb
+++ db/schema.rb
@@ -52,23 +52,7 @@ ActiveRecord::Schema.define(version: 2020_01_18_235557) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "stripe_id"
-    t.string "stripe_subscription_id"
-    t.string "card_last4"
-    t.integer "card_exp_month"
-    t.integer "card_exp_year"
-    t.string "card_type"
     t.boolean "admin"
     t.boolean "subscribed"
     t.index ["email"], name: "index_users_on_email", unique: true
