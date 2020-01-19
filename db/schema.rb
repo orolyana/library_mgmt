80d32d23206d72bc127d0f11e24b014af3a66e1b
orolyana <ibrahimabas97@gmail.com>
2020-01-19 14:59:20 +0300

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_235557) do
+ActiveRecord::Schema.define(version: 2020_01_18_235557) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
